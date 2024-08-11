5013891dfda28883eb582af587b2abf3d97cf3c8
it21263576 <it21263576@my.sliit.lk>
2024-08-11 18:23:22 +0530

    responsive navbar changes

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 
+import Adobe6 from "../assets/Adobe6.png";
+import favourite from "../assets/favourite.png";
+import shoppingBag from "../assets/shoppingBag.png";
+
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -11,9 +15,9 @@ const Navbar = () => {
     <header className="fixed shadow-custom-shadow top-0 z-40 w-full backdrop-blur-sm">
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-medium whitespace-nowrap text-gray-500">Dress<strong className='text-black'>Zone</strong></span>
+          <a href="https://flowbite.com/" className="flex items-center space-x-1 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 lg:h-8 2xl:h-8" alt="Flowbite Logo" />
+            <span className="self-center lg:text-2xl 2xl:text-2xl font-medium whitespace-nowrap text-gray-500">Dress<strong className='text-black'>Zone</strong></span>
           </a>
           <div className="flex md:order-2">
             {/* Search Bar */}
@@ -22,11 +26,11 @@ const Navbar = () => {
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
               aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
+              className="xl:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-2"
             >
               <svg
                 className="w-5 h-5"
-                aria-hidden="true"
+                aria-hidden="false"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 20 20"
@@ -41,7 +45,8 @@ const Navbar = () => {
               </svg>
               <span className="sr-only">Search</span>
             </button>
-            <div className="relative hidden md:block">
+            
+            <div className="relative hidden xl:block">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
                   className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -67,6 +72,50 @@ const Navbar = () => {
                 placeholder="Search..."
               />
             </div>
+
+
+            <div class="flex items-center mt-1 lg:ml-8 2xl:ml-14 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button type="button" class="flex text-sm md:me-0 focus:ring-1 rounded-full focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                  <span class="sr-only">Open user menu</span>
+                  <img class="w-5 h-5 lg:w-8 lg:h-8 2xl:w-6 2xl:h-6 " src={favourite} alt="user photo"/>
+                </button>
+            </div>
+
+            <div class="flex items-center ml-1 md:ml-1 lg:ml-3 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button type="button" class="flex text-sm md:me-0 focus:ring-1 rounded-full focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                  <span class="sr-only">Open user menu</span>
+                  <img class="w-6 h-6 lg:w-10 lg:h-10 2xl:w-8 2xl:h-8" src={shoppingBag} alt="user photo"/>
+                </button>
+            </div>
+
+            <div class="flex items-center ml-2 mr-1 md:ml-5 lg:ml-8 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                  <span class="sr-only">Open user menu</span>
+                  <img class="w-5 h-5 lg:w-10 lg:h-10 2xl:w-8 2xl:h-8 rounded-full" src={Adobe6} alt="user photo"/>
+                </button>
+              
+              <div class="absolute hidden z-50 lg:right-2 2xl:right-16 top-20 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                <div class="px-4 py-3">
+                  <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                  <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                </div>
+                <ul class="py-2" aria-labelledby="user-menu-button">
+                  <li>
+                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                  </li>
+                </ul>
+              </div>
+          </div>
+
             {/* Dropdown Button */}
             <button
               data-collapse-toggle="navbar-search"
@@ -93,11 +142,12 @@ const Navbar = () => {
               </svg>
             </button>
           </div>
+          
           <div
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-search"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col lg:text-2xl 2xl:text-lg p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
                   href="#"
